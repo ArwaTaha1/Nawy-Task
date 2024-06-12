e31d932ca554cabdf93027d9ff6cfc05975fa933
ArwaTaha1 <155313303+ArwaTaha1@users.noreply.github.com>
2024-06-13 00:18:55 +0300

    final commit

---
+++ Question1.sql
@@ -0,0 +1,1 @@
+Select c.name, max(r.ranking), count(s.*) from colleges c, students s, rankings r where year = 2015 and c.id = s.collegeId and r.studentId = s.id and r.ranking between 1 and 3 group by c.name 
---
+++ Question2&Question3.java
@@ -0,0 +1,69 @@
+package arwa;
+import java.util.*;
+public class test {
+	
+	public static ArrayList<String> transformDateFormat(String [] dates) {
+		ArrayList<String> result = new ArrayList<String>();
+		for (int i =0;i<dates.length;i++) {
+			String date = dates[i];
+			if (dates[i].length()<10) 
+				continue;
+			if (date.charAt(4)=='/' || date.charAt(4)=='-') {
+				if (Integer.parseInt(date.substring(5,7))>12) {
+					String s = date.substring(0,4) + date.substring(8) + date.substring(5,7);
+					result.add(s);	
+				}
+				else {
+					String s = date.substring(0,4)  + date.substring(5,7) + date.substring(8);
+					result.add(s);
+				}
+			}
+			else {
+				if (Integer.parseInt(date.substring(0,2))>12) {
+					String s = date.substring(6) + date.substring(3,5) + date.substring(0,2);
+					result.add(s);	
+				}
+				else {
+					String s = date.substring(6) + date.substring(0,2) + date.substring(3,5);
+					result.add(s);
+				}
+				
+			}
+			
+		}
+		return result;
+		
+		}
+	
+	
+	public static boolean isValid(String s) {
+		if (s.length()%2==1)
+			return false;
+		Hashtable <Character,Character> h = new Hashtable<Character,Character> ();
+		h.put('(', ')');
+		h.put('[', ']');
+		h.put('{', '}');
+		Stack <Character> stack = new Stack<Character>();
+		for (int i =0;i<s.length();i++) {
+			char tmp = s.charAt(i);
+			if (!h.containsKey(tmp)&&!h.containsValue(tmp))
+				return false;
+			if (h.containsKey(tmp)) { //{{{()[]}}}
+				stack.push(tmp);
+			}
+			if (h.containsValue(tmp)&& h.get(stack.peek())==tmp) {
+				stack.pop();
+			}
+			
+			
+		}
+		return stack.isEmpty();
+	}
+	
+	public static void main(String [] args) {
+		String [] tmp = {"2010/02/20", "19/12/2016", "11-18-2012", "20130720","30-12-2012", "2024/20/11" };
+		System.out.println(transformDateFormat(tmp));
+	}
+	
+
+}
