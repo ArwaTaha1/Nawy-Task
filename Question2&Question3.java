b1e74c8f18b2b7e0515dd8f9f09f0583dab735df
ArwaTaha1 <155313303+ArwaTaha1@users.noreply.github.com>
2024-06-13 00:46:12 +0300

    commit

--- Question2&Question3.java
+++ Question2&Question3.java
@@ -3,28 +3,34 @@ import java.util.*;
 public class test {
 	
 	public static ArrayList<String> transformDateFormat(String [] dates) {
-		ArrayList<String> result = new ArrayList<String>();
+		ArrayList<String> result = new ArrayList<String>(); //["2010/02/20", "19/12/2016", "11-18-2012", "20130720"]
 		for (int i =0;i<dates.length;i++) {
-			String date = dates[i];
+			String date = dates[i]; //YYYY/MM/DD, DD/MM/YYYY, and MM-DD-YYYY
 			if (dates[i].length()<10) 
 				continue;
-			if (date.charAt(4)=='/' || date.charAt(4)=='-') {
+			if (date.charAt(4)=='/') {
 				if (Integer.parseInt(date.substring(5,7))>12) {
-					String s = date.substring(0,4) + date.substring(8) + date.substring(5,7);
-					result.add(s);	
+					continue;
 				}
 				else {
 					String s = date.substring(0,4)  + date.substring(5,7) + date.substring(8);
 					result.add(s);
 				}
 			}
-			else {
-				if (Integer.parseInt(date.substring(0,2))>12) {
-					String s = date.substring(6) + date.substring(3,5) + date.substring(0,2);
-					result.add(s);	
-				}
+			else if (date.charAt(2)=='-') {
+				if (Integer.parseInt(date.substring(0,2))>12)
+					continue;
 				else {
 					String s = date.substring(6) + date.substring(0,2) + date.substring(3,5);
+					result.add(s);	
+				}
+			}
+			else if (date.charAt(2)=='/') {
+				if (Integer.parseInt(date.substring(3,5))>12) {
+						continue;
+				}
+				else {
+					String s = date.substring(6) + date.substring(3,5) + date.substring(0,2);
 					result.add(s);
 				}
 				
